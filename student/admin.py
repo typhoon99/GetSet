6e9c646b6a291564c9201d49623c06d8d29020e1
anmol2406 <dusejaanmol7@gmail.com>
2020-03-16 20:27:39 +0530

    Created models Students, Guide, Topics, Group,Report, PPT

--- student/admin.py
+++ student/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile,Students,Guide,Topic,Report,PPT
+
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(Students)
+admin.site.register(Guide)
+admin.site.register(Topic)
+admin.site.register(Report)
+admin.site.register(PPT)
--- student/models.py
+++ student/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
 class UserProfile(models.Model):
@@ -17,7 +18,70 @@ class UserProfile(models.Model):
     bio=models.TextField()
     image=models.ImageField(upload_to='images/')
     groupid=models.IntegerField()
+    created_on=models.DateTimeField(default=timezone.now)
+    
 
     def __str__(self):
         return str(self.firstname + " " +self.lastname)
+
+class Students(models.Model):
+    rno=models.IntegerField()
+    firstname=models.CharField(max_length=50)
+    lastname=models.CharField(max_length=50)
+    mobileno=models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return str(self.firstname + " " +self.lastname)
+
+class Guide(models.Model):
+    firstname=models.CharField(max_length=50)
+    lastname=models.CharField(max_length=50)
+    mobileno=models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return str(self.firstname + " " +self.lastname)
+
+class Topic(models.Model):
+    topicName=models.CharField(max_length=30)
+    domain=models.CharField(max_length=30)
+    description=models.CharField(max_length=80)
+    document=models.FileField(upload_to ='uploads/')
+    created_on=models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.topicName
+
+
+class Group(models.Model):
+    groupid=models.IntegerField(primary_key=True)
+    user1=models.ForeignKey(UserProfile,on_delete=models.CASCADE, related_name = "member1")
+    user2=models.ForeignKey(UserProfile,on_delete=models.CASCADE, related_name = "member2")
+    user3=models.ForeignKey(UserProfile,on_delete=models.CASCADE, related_name = "member3")
+    user4=models.ForeignKey(UserProfile,on_delete=models.CASCADE, related_name = "member4")
+    guide=models.ForeignKey(UserProfile,on_delete=models.CASCADE)
+    topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    created_on=models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.groupid
+    
+class Report(models.Model):
+    report=models.FileField(upload_to ='reports/')
+    description=models.TextField(blank=True)
+    groupid=models.ForeignKey(Group,on_delete=models.CASCADE)
+    comment=models.TextField()
+
+    def __str__(self):
+        return str(self.groupid + " " +self.report)
+
+class PPT(models.Model):   
+    ppt=models.FileField(upload_to ='presentations/')
+    description=models.TextField(blank=True)
+    groupid=models.ForeignKey(Group,on_delete=models.CASCADE)
+    comment=models.TextField()
+
+    def __str__(self):
+        return str(self.groupid + " " +self.ppt)
+
 

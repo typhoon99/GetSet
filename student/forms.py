d2ebbec3f965d8f168dd04810c85d5938134617c
anmol2406 <dusejaanmol7@gmail.com>
2020-03-18 16:54:07 +0530

    Created signup form

--- student/forms.py
+++ student/forms.py
@@ -3,9 +3,12 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class SignUpForm(UserCreationForm):
-    mobileno = forms.IntegerField()
+class SignUp(forms.Form):
+    username = forms.CharField(label='username')
+    email = forms.EmailField(label='email')
+    password1 = forms.CharField(label='password1')
+    password2 = forms.CharField(label='password2')
     
     class Meta:
-        model = User, UserProfile
-        fields = ['rollno','firstName','lastName','year','division','mobileno','username','password1','password2',]
+        model = User
+        fields = ['username','email','password1','password2',]
--- student/views.py
+++ student/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django import forms
-from .forms import SignUpForm
+from .forms import SignUp
 from django.contrib.auth import login, authenticate
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def login(request):
@@ -9,20 +10,15 @@ def login(request):
 
 def signup(request):
 	if request.method == "POST":
-		form = SignUpForm(request.POST)
+		form = SignUp(request.POST)
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
 			raw_password = form.cleaned_data.get('password1')
 			email=form.cleaned_data.get('email')
 			user = authenticate(username = username, password = raw_password)
-			firstName=form.cleaned_data.get('firstName')
-			lastName=form.cleaned_data.get('lastName')
-			rollno=form.cleaned_data.get('rollNo')
-			year=form.cleaned_data.get('year')
-			division=form.cleaned_data.get('division')
-			login(request, user)
-			return redirect('ungrouped')
+			login(request,user)
+			return HttpResponseRedirect('/ungrouped/')
 	else:
-		form = SignUpForm()
+		form = SignUp()
 	return render(request, 'student/signup.html', {'form':form})

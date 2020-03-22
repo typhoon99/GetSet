01cb5e1c3c1042b7295d3f90ce0b11ee44890a22
Pushkar A <asapure.pushkar.004@gmail.com>
2020-03-22 21:54:10 +0530

    Resolved merge conflicts

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(' ', include('student.urls')),
+    path('', include('student.urls')),
     path('accounts/login/',views.LoginView.as_view(),name='login'),
 ]
